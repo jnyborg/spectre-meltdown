7df6c357529ee78aab826c36cfa590e72f3c97b1
Joachim Nyborg <joachim.c.nyborg@gmail.com>
2018-05-16 16:11:39 +0200

    Spectre

---
+++ Makefile
@@ -0,0 +1,2 @@
+all:
+	gcc -o spectre.o spectre.c
---
+++ spectre.c
@@ -0,0 +1,10 @@
+//
+// Created by Joachim Nyborg on 16/05/2018.
+//
+
+
+int main(int argc, char const *argv[])
+{
+    printf("Hello world");
+    return 0;
+}
