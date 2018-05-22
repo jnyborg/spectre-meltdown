0bfb0d9392ab71dbcbe3cc04d178bc19af65e67b
Joachim Nyborg <joachim.c.nyborg@gmail.com>
2018-05-22 14:58:42 +0200

    setup.py

--- setup.py
+++ setup.py
@@ -2,4 +2,4 @@ from distutils.core import setup, Extension
 
 setup(name='Spectre',
       version='1.0',
-      ext_modules=[Extension('pyspectre', sources=['pyspectre.c'])])
+      ext_modules=[Extension('pyspectre', sources=['pyspectre.c'], extra_compile_args=["-std=c99"])])
